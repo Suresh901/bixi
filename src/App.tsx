629b25529292a0346f5f84652c79e3eb3b438678
Suresh901 <sureshshrestha915@gmail.com>
2025-02-17 15:00:19 +0545

    route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import Homepage from './pages/homepage/Homepage'
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Homepage from './pages/homepage/Homepage';
+import Products from './pages/products/Products';
+import Product from './pages/product/Product';
 
 function App() {
   return (
-    <Homepage />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/product/:id' element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
